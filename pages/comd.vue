b544540e863a50b2ba92bdfc24e7156453710f24
DustBlower <shinya_17n1100095@nnn.ed.jp>
2021-10-28 01:38:47 +0900

    subcreate

---
+++ pages/comd.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="container">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="js/hum2.js"></script>	
+<div class="feader">
+	<div class="hanberger_meu"></div>
+			<h1 class="flex_c title">Home</h1>
+	<div class="hamburger">
+  <span></span>
+  <span></span>
+  <span></span>
+</div>
+
+<nav class="globalMenuSp">
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="CAte.html">Category</a></li>
+    </ul>
+</nav>
+</div>
+		
+	<div class="contents">
+		<!---タイトル-->
+	  <div class="image01"></div>
+	  <h1 class="title01">Communication Design ~縁~</h1>
+		<p class="intro">********************************************************<br>
+		************************************************************<br>
+		***********************************************************</p>
+	</div>
+		<div class="main_image">
+	
+	
+	</div>
+	
+	<div class="image02"><img src="asset24.svg" alt=""/></div>
+	<div class="LEARNMORE">
+		<a href="CAte.html" id="CATEGORY"><img src="learnmore.svg" alt=""/></a>
+	</div>
+<div class="image03">
+	<img src="asset25.svg" alt=""/></div>
+<div class="flex_r p"> <p>*********<br>*************<br>*********</p> </div>
+	
+<div class="footer">	
+  <div class="border01"></div>
+  <div class="border02"></div>
+  <div class="border03"></div>
+	<div class="border001"></div>
+	<div class="border04">
+	  <a id="INFO"><h1 class="INFO">INFO</h1></a>
+	<div class="flex_j link">
+	<div>
+    <a href="#"><img class="border_image01" src="images/image07.jpg" width="250" height="255" alt=""/> 
+		<p class="flex_c font">aaaaa</p>
+		</a>
+	
+	</div>
+	<div>
+	<a href="#"><img class="border_image01 " src="images/image07.jpg" width="250" height="255" alt=""/> 
+	<p class="flex_c font">aaaaa</p>
+		</a>
+	</div>
+	<div>
+	<a href="#"><img class="border_image01" src="images/image07.jpg" width="250" height="255" alt=""/>
+	<p class="flex_c font">aaaaa</p>	
+	</a>
+	</div>
+	</div>
+	</div>
+	<div class="border002"></div>
+	<div class="background01">
+	<div class="border06"></div>
+		<div class="background02">
+	<div class="border07"></div>
+	<div class="border08"></div>
+  <div class="border09"></div>
+  <div class="border10"></div>
+  <div class="border11"></div>
+  <div class="border12"></div>
+  <div class="border13"></div>
+  <div class="border14"></div>
+  <div class="border15"></div>
+  <div class="border16"></div>
+  <div class="border17"></div>
+  <div class="border18"></div>
+  <div class="border19"></div>
+  <div class="border20"></div>
+		</div>
+		</div>
+	
+	<div class="box19">
+		<a id="CONTACT">
+   <p class="maile">　　　　　お問い合わせはこちらから<br>20md013@st.nuas.ac.jp</p></a>
+</div>	
+	<div class="parent">
+	   <a href="#CATEGORY"> <h1>CATEGORY</h1></a>
+		<a href="#INFO"><h1>INFO</h1></a>
+		<a href="#CONTACT"><h1>CONTACT</h1></a>
+	</div>
+	<div class="bar"></div>
+      </div>
+</div>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+.container {
+
+  }
+</style>
---
+++ pages/zoukei.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="zoukei">
+    <a href="https://www.instagram.com/nuas_de.de.de.21/" target="_blank"><div class="insta"></div></a>
+    <h1 class="omTitle">ワンマンスチャレンジ</h1>
+  <video src="../assets/video.mp4" class="kaboM" autoplay muted controls></video>
+    <!-- <div class="movie">
+        <iframe class="videoM"  src="https://www.youtube.com/embed/gb6RcazTL0o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div> -->
+</div>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap');
+$Dred: #EC6B4D;
+
+.zoukei {
+    font-family: 'Noto Sans JP', sans-serif;
+    position: relative;
+    height: 100vh;
+    width: 100%;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    .omTitle{
+      top: 0vh;
+      color:$Dred;
+      font-size: 80px;
+    }
+    .insta{
+      position: fixed;
+      top:30px;
+      right:30px;
+      height: 50px;
+      width: 50px;
+      background:url("assets/img/instaRGB.png")no-repeat;
+      background-size: cover;
+      cursor: pointer;
+      z-index: 5;
+    }
+    .kaboM{
+      top: 20vh;
+      border:15px solid $Dred;
+      height: 70vh;
+      width: 70vw;
+      display: block;
+    }
+    .movie{
+      width: 100%;
+    .videoM{
+      height: 800px;
+      width: 80%;
+    }
+    }
+  }
+</style>
